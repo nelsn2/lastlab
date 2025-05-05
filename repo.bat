611944dd4ae085f6cc0dadb7119e99f92ea17ce0
nelsn2 <nelsn2@uis.edu>
2025-05-04 22:25:03 -0500

    initial commit

---
+++ repo.bat
@@ -0,0 +1,7 @@
+echo "# lastlab" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nelsn2/lastlab.git
+git push -u origin main
